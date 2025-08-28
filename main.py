010618022ea7285cab1ea1f265420a22c03000c8
夔嶷 <2195357023@qq.com>
2025-08-28 10:32:43 +0800

    更新项目说明

--- main.py
+++
@@ -1,9 +0,0 @@
-api_urls = {
-        'api1': 'https://api.btstu.cn/sjbz/api.php',        # 随机各类壁纸
-        'api2': 'https://api.paugram.com/wallpaper/',    # 随机动漫壁纸
-        'api3': 'https://cdn.seovx.com/d/?mom=302',      # 随机二次元
-        'api4': 'https://cdn.seovx.com/?mom=302',        # 随机美图
-        'api5': 'https://cdn.seovx.com/ha/?mom=302',     # 随机古风
-        'api6': 'https://www.dmoe.cc/random.php',        # 樱花二次元
-    }
-print(list(api_urls.keys()))
